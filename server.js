cec19dd26155d9894a263b4446898b5bc4afb145
Klement Omeri <klementomeri97@gmail.com>
2021-01-23 19:15:57 +0100

    ci: force https on express server

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
 const express = require('express');
-
 const app = express();
-
+app.use(requireHTTPS);
 app.use(express.static('./dist/kral-kutu-frontend'));
 
 app.get('/*', (req, res) =>
