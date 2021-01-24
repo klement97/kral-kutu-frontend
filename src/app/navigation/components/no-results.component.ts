c754fec1a8ce8ee7603db9c3ca17db70699c62c9
Klement Omeri <klementomeri97@gmail.com>
2021-01-24 02:22:21 +0100

    feat: new design for no results component

--- src/app/navigation/components/no-results.component.ts
+++ src/app/navigation/components/no-results.component.ts
@@ -5,10 +5,38 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-no-results',
   template: `
       <ng-container *transloco="let t">
-          <p>{{t('no results')}}</p>
+          <div class="no-results">
+              <mat-divider></mat-divider>
+              <div class="message">
+                  <mat-icon>info</mat-icon>&nbsp;
+                  <p>{{t('no results message')}}</p>
+              </div>
+              <mat-divider></mat-divider>
+          </div>
       </ng-container>
   `,
-  styles: []
+  styles: [`
+      .no-results {
+          color: #00a81c;
+      }
+
+      .no-results > mat-divider {
+          background: #00a81c;
+      }
+
+      .no-results > div > p {
+          /*override global style that gives 12px bottom margin to p tag*/
+          margin: 0;
+      }
+
+      .message {
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          align-items: center;
+          padding: 15px 0;
+      }
+  `]
 })
 export class NoResultsComponent implements OnInit {
 
